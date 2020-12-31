f239d7dbbce1b49cb17a0be1fdf91940cd77f506
Sergey <siniiitsa@gmail.com>
2020-12-31 21:04:05 +0200

    each

---
+++ arrays/each.js
@@ -0,0 +1,24 @@
+const _ = require('lodash');
+
+const nums = [1, 2, 3];
+
+// native forEach returns undefined
+const nativeReturn = nums.forEach((n) => {
+  console.log(n);
+});
+
+// lodash each return original array so we can chain other methods
+const lodashReturn = _.each(nums, (n) => {
+  console.log(n);
+});
+
+// lodash each also works with objects
+const users = {
+  id_1: 'Sergey',
+  id_2: 'Olga',
+  id_3: 'Vlad',
+};
+
+_.each(users, (value, key, third) => {
+  console.log(`${key}: ${value}`);
+});
